3cb430a050ab4e05918ea9114ba38d260f1a8390
Harsh <harshsik7890@gmail.com>
2024-11-25 12:59:49 +0530

    ..

--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -1,11 +1,42 @@
+import Unauthorized from '@/components/unauthorized'
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const SubAccount = () => {
-  return (
-    <div>
-      SubAccount
-    </div>
-  )
+type Props = {
+  searchParams: { state: string; code: string }
 }
 
-export default SubAccount
+const SubAccountMainPage = async ({ searchParams }: Props) => {
+  const {state , code}  = await searchParams;
+  const agencyId = await verifyAndAcceptInvitation()
+
+  if (!agencyId) {
+    return <Unauthorized />
+  }
+
+  const user = await getAuthUserDetails()
+  if (!user) return
+
+  const getFirstSubaccountWithAccess = user.Permissions.find(
+    (permission) => permission.access === true
+  )
+
+  if (state) {
+    const statePath = state.split('___')[0]
+    const stateSubaccountId = state.split('___')[1]
+    if (!stateSubaccountId) return <Unauthorized />
+    return redirect(
+      `/subaccount/${stateSubaccountId}/${statePath}?code=${code}`
+    )
+  }
+
+  if (getFirstSubaccountWithAccess) {
+    return redirect(`/subaccount/${getFirstSubaccountWithAccess.subAccountId}`)
+  }
+
+  console.log("hello")
+  return <Unauthorized />
+}
+
+export default SubAccountMainPage
